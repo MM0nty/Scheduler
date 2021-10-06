fe7f4ef7a5778ecdc42c4fdfd699d9e94e648493
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-10-06 19:50:05 -0400

    Add successful button tests and import classNames library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,15 @@
 import React from "react";
 
 import "components/Button.scss";
+const classNames = require("classnames")
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
 
    return (
-      <Button disabled onClick={action("button-clicked")}>Disabled</Button>
-   )
+      <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
+   );
 }
