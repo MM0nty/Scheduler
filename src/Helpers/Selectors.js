5e907edf44a54e11abffd0569a82e2f2899875e4
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-10-06 21:56:52 -0400

    Add getAppointments, getInterviewers, and getInterview Helper functions

---
+++ src/Helpers/Selectors.js
@@ -0,0 +1,35 @@
+export function getAppointments(state, dayInput) {
+  const filteredDays = state.days.filter(day => day.name === dayInput)
+  const day = filteredDays[0]
+  if (!day) {
+    return []
+  }
+  const filteredAppointments = day.appointments.map(appointment => {
+    return state.appointments[appointment]
+  })
+  return filteredAppointments
+}
+
+export function getInterviewers(state, dayInput) {
+  const filteredDays = state.days.filter(day => day.name === dayInput)
+  const day = filteredDays[0]
+  if (!day) {
+    return []
+  }
+  const filteredInterviewers = day.interviewers.map(interviewer => {
+    return state.interviewers[interviewer]
+  })
+  return filteredInterviewers
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null
+  }
+  const interviewObject = {
+    "interviewer":
+      state.interviewers[interview.interviewer],
+    "student": interview.student
+  }
+  return interviewObject
+}
