b9b4b6597ec438e2fca45eb771504c9853ec9bba
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-10-10 13:42:54 -0400

    Make updateSpots its own function

--- src/Hooks/useApplicationData.js
+++ src/Hooks/useApplicationData.js
@@ -2,7 +2,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData(props) {
-  const [state, setState] = useState({ day: "Monday", days: [], appointments: {}, interviewers: {} });
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
   const setDay = day => setState({ ...state, day: day });
 
   function bookInterview(id, interview) {
@@ -11,25 +16,14 @@ export default function useApplicationData(props) {
       ...state.appointments[id],
       interview: { ...interview }
     };
+
     // updates appointments array
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-    // finds the day's schedule
-    const findDay = state.days.findIndex((day) =>
-      day.appointments.includes(id));
-    let day;
-    // books appointment if interview is null
-    if (!state.appointments[id].interview) {
-      day = {
-        ...state.days[findDay],
-        spots: state.days[findDay].spots - 1
-      }
-    };
-    const days = [...state.days];
-    // updates spots remaining in sidebar
-    days.splice(findDay, 1, day);
+    const days = updateSpots(id);
+
     return axios.put(`/api/appointments/${id}`, { interview })
       .then(() =>
         setState({ ...state, appointments, days }));
@@ -41,28 +35,44 @@ export default function useApplicationData(props) {
       ...state.appointments[id],
       interview: null
     };
+
     // updates appointments array
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-    // finds the day's schedule
-    const findDay = state.days.findIndex((day) =>
-      day.appointments.includes(id));
-    let day;
-    // cancels appointment if interview is not null
-    if (state.appointments[id].interview) {
-      day = {
-        ...state.days[findDay],
-        spots: state.days[findDay].spots + 1
-      };
-    };
-    const days = [...state.days];
-    // updates spots remaining in sidebar
-    days.splice(findDay, 1, day);
+    const days = updateSpots(id);
+
     return axios.delete(`/api/appointments/${id}`, appointment)
       .then(() =>
         setState({ ...state, appointments, days }));
+  }
+
+  function updateSpots(id) {
+    // finds the day's schedule
+    const getDaysSchedule = state.days.findIndex((day) =>
+      day.appointments.includes(id));
+    let day;
+
+    // books appointment if interview is null
+    if (!state.appointments[id].interview) {
+      day = {
+        ...state.days[getDaysSchedule],
+        spots: state.days[getDaysSchedule].spots - 1
+      }
+    };
+
+    // cancels appointment if interview is not null
+    if (state.appointments[id].interview) {
+      day = {
+        ...state.days[getDaysSchedule],
+        spots: state.days[getDaysSchedule].spots + 1
+      };
+    };
+    const days = [...state.days];
+
+    days.splice(getDaysSchedule, 1, day); // updates spots remaining
+    return days;
   }
 
   useEffect(() => {
@@ -71,7 +81,12 @@ export default function useApplicationData(props) {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers"),
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
     });
   }, []);
 
