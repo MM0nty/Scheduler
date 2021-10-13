9b12b229d1ded5ecb6f43a9814f985acd1f54f1b
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-10-12 20:46:01 -0400

    Add mock axios with Application.test

--- src/Components/__tests__/Application.test.js
+++ src/Components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "Components/Application";
 
@@ -9,3 +9,23 @@ afterEach(cleanup);
 it("renders without crashing", () => {
   render(<Application />);
 });
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
+/*
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
+*/
